8fb92c5419648d2284412e79c992af0cfc70b1f2
Reyaad Rafeek <reyaad510@gmail.com>
2019-06-20 22:39:14 -0400

    look at notes

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import 'jest-dom/extend-expect';
+// below will clean things up for us
+import '@testing-library/react/cleanup-after-each';
 
 xit('renders without crashing', () => {
   const div = document.createElement('div');
@@ -12,4 +15,21 @@ xit('renders without crashing', () => {
   it('renders without crashing', () => {
     render(<App />)
   });
+
+  it('displays hello world', () => {
+    const { queryByText } = render(<App />);
+    const header = queryByText(/hello world/i)
+    // const { getByText } = render(<App />);
+    // const header = getByText(/hello world/i);
+    expect(header).toBeVisible();
+  });
+
+  it('displays greeting when button is clicked', () => {
+    const { getByText } = render(<App />);
+    const button = getByText(/greet/i);
+
+    fireEvent.click(button);
+
+    getByText(/hello reyaad/i);
+  })
 
